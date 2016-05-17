8ed08b71fafeb5b1131a524201cb206c8956710b
SoniaHorchidan <sonia.florina@yahoo.com>
2016-05-17 20:29:06 +0300

    Add files via upload

---
+++ Caine.cpp
@@ -0,0 +1,19 @@
+#include "Caine.h"
+#include <string.h>
+
+Caine::Caine()
+{
+    IDAnimal=11;
+}
+
+std::string Caine::facCaAnimalu()
+{
+    return " Woof!";
+}
+
+int Caine::getIDAnimal(){
+    return 11;
+}
+
+
+
